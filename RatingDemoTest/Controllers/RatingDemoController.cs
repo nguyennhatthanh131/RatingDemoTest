88586b03f04196a06fd03387c1ba88dbd985e8ad
nguyennhatthanh131 <harrynguyen131@gmail.com>
2020-05-17 03:08:22 +0700

    able to change back and forth between different services.

--- RatingDemoTest/Controllers/RatingDemoController.cs
+++ RatingDemoTest/Controllers/RatingDemoController.cs
@@ -17,5 +17,39 @@ namespace RatingDemoTest.Controllers
         {
             return PartialView("_Login");
         }
+
+        public IActionResult Rating(string passCode, int serviceId = 1)
+        {
+            var isAbleToLogin = AuthenLogin(passCode);
+            if (isAbleToLogin)
+            {
+                var pageToLoad = "_SanitationRating";
+
+                switch (serviceId)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        pageToLoad = "_SecurityRating";
+                        break;
+                    case 3:
+                        pageToLoad = "_StudentCareRating";
+                        break;
+                    default:
+                        break;
+                }
+
+                return PartialView(pageToLoad);
+            }
+
+            return Json(isAbleToLogin);
+
+        }
+
+        private bool AuthenLogin(string passCode)
+        {
+            if (passCode == null || passCode == string.Empty)                  return false;
+            return passCode.Equals("123456789");
+        }
     }
 }
